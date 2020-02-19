3858cd8ed60d386b8bd0b6c4022655359457bc9f
Michael Bessolov <mbessolov@users.noreply.github.com>
2020-02-19 15:46:31 -0800

    Lamiaceae genera

---
+++ src/Flipsum/Data/Lamiaceae.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace Flipsum\Data;
+
+final class Lamiaceae
+{
+    /**
+     * @return string[]
+     */
+    public static function getGenera(): array
+    {
+        return [
+            'Acanthomintha','Achyrospermum','Acinos','Acrocephalus','Acrotome','Acrymia','Adelosa','Aegiphila',
+            'Aeollanthus','Agastache','Ajuga','Ajugoides','Alajja','Alvesia','Amasonia','Amethystea','Anisochilus',
+            'Anisomeles','Asterohyptis','Ballota','Basilicum','Becium','Benguellia','Blephilia','Bostrychanthera',
+            'Bovonia','Brachysola','Brazoria','Bystropogon','Calamintha','Callicarpa','Capitanopsis','Capitanya',
+            'Caryopteris','Catoferia','Cedronella','Ceratanthus','Chaiturus','Chamaesphacos','Chaunostoma',
+            'Chelonopsis','Chloanthes','Cleonia','Clerodendrum','Clinopodium','Colebrookea','Collinsonia',
+            'Colquhounia','Comanthosphace','Congea','Conradina','Coridothymus','Cornutia','Craniotome','Cryphia',
+            'Cuminia','Cunila','Cyanostegia','Cyclotrichium','Cymaria','Dauphinea','Dicerandra','Dicrastylis',
+            'Discretitheca','Dorystoechas','Dracocephalum','Drepanocaryum','Elsholtzia','Endostemon','Englerastrum',
+            'Eremostachys','Eriope','Eriophyton','Eriopidion','Eriothymus','Erythrochlamys','Euhesperida','Eurysolen',
+            'Faradaya','Fuerstia','Galeopsis','Garrettia','Geniosporum','Glechoma','Glechon','Glossocarya','Gmelina',
+            'Gomphostemma','Gontscharovia','Hanceola','Haplostachys','Haumaniastrum','Hedeoma','Hemiandra','Hemigenia',
+            'Hemiphora','Hemizygia','Hesperozygis','Heterolamium','Hoehnea','Holmskioldia','Holocheila','Holostylon',
+            'Horminum','Hosea','Hoslundia','Huxleya','Hymenocrater','Hymenopyramis','Hypenia','Hypogomphia',
+            'Hyptidendron','Hyptis','Hyssopus','Isodictyophorus','Isodon','Isoleucas','Kalaharia','Karomia','Keiskea',
+            'Killickia','Kudrjaschevia','Kurzamra','Lachnostachys','Lagochilus','Lagopsis','Lallemantia','Lamiophlomis',
+            'Lamium','Lavandula','Leocus','Leonotis','Leonurus','Lepechinia','Leucas','Leucophae','Leucosceptrum',
+            'Limniboza','Lophanthus','Loxocalyx','Lycopus','Macbridea','Madlabium','Mallophora','Marmoritis',
+            'Marrubium','Marsypianthes','Matsumurella','Meehania','Melissa','Melittis','Mentha','Meriandra','Mesona',
+            'Metastachydium','Microcorys','Micromeria','Microtoena','Minthostachys','Moluccella','Monarda',
+            'Monardella','Monochilus','Mosla','Neoeplingia','Neohyptis','Neorapinia','Nepeta','Newcastelia','Nosema',
+            'Notochaete','Obtegomeria','Ocimum','Octomeron','Ombrocharis','Oncinocalyx','Origanum','Orthosiphon',
+            'Otostegia','Ovieda','Oxera','Panzerina','Paralamium','Paraphlomis','Paravitex','Peltodon','Pentapleura',
+            'Perilla','Perillula','Peronema','Perovskia','Perrierastrum','Petitia','Petraeovitex','Phlomidoschema',
+            'Phlomis','Phlomoides','Phyllostegia','Physopsis','Physostegia','Piloblephis','Pitardia','Pityrodia',
+            'Platostoma','Plectranthus','Pogogyne','Pogostemon','Poliomintha','Prasium','Premna','Prostanthera',
+            'Prunella','Pseuderemostachys','Pseudocarpidium','Pseudocaryopteris','Pseudomarrubium','Puntia',
+            'Pycnanthemum','Pycnostachys','Rabdosiella','Renschia','Rhabdocaulon','Rhaphiodon','Rhododon','Rosmarinus',
+            'Rostrinucula','Rotheca','Roylea','Rubiteucris','Rydingia','Sabaudia','Saccocalyx','Salazaria','Salvia',
+            'Satureja','Schizonepeta','Schnabelia','Scutellaria','Sideritis','Siphocranion','Solenostemon',
+            'Spartothamnella','Sphenodesme','Stachydeoma','Stachyopsis','Stachys','Stenogyne','Sulaimania','Suzukia',
+            'Symphorema','Symphostemon','Synandra','Syncolostemon','Tectona','Teijsmanniodendron','Tetraclea',
+            'Tetradenia','Teucridium','Teucrium','Thorncroftia','Thuspeinanta','Thymbra','Thymus','Tinnea',
+            'Trichostema','Tripora','Tsoongia','Vitex','Viticipremna','Volkameria','Warnockia','Wenchengia',
+            'Westringia','Wiedemannia','Wrixonia','Xenopoma','Zataria','Zhumeria','Ziziphora'
+        ];
+    }
+}
